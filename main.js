6be9b073a128ffa118d7949436d33c1a8bcab037
wrevans98 <wrevans98@gmail.com>
2024-05-08 20:52:37 -0500

    Javascript Smooth Scrolling Functions and Thank You Message Weight Change

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function myFunction() {
-    document.getElementById("thankYou").innerHTML = "Thank you for contacting me!"
+    document.getElementById("thankYou").innerHTML = "Thank you for contacting me! Can't wait to connect soon."
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('click', function (e) {
+        if (e.target.matches('a[href^="#"]')) {
+            e.preventDefault();
+
+            const target = document.querySelector(e.target.getAttribute('href'));
+
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
